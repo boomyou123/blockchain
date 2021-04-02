0fbfa7791435ab4f0488403c740669f92a3ab64a
boomyou123 <62646708+boomyou123@users.noreply.github.com>
2021-04-02 13:14:53 +0800

    Add files via upload

---
+++ dr 1/xampp/name.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+</style>
+</head>
+<div class="row">
+	<div class="col-1">
+		<img src="https://github.com/boomyou123/blockchain.git/IMG_20210401_142929.jpg" class="jpg">
+	</div>
+	<h2>
+		My name is Bong Kai Sheng, and my college is Kolej Synergy. My hobby is playing games. Although my ability is a bit weak, I will still try my best to learn new skills.
+	</h2>
+	<div class="box1">
+	<span><strong>Birth day:</strong>7 Feb 2000</span>
+	<span><strong>Age:</strong>21</span>
+	<span><strong>Phone:</strong>011-55679917</span>
+	<span><strong>City:</strong>Nibong Tebal</span>
+	<span><strong>PhEmailone</strong>boomkaisheng123@gmail.com</span>
+</div>
+</div>
+</html>
+
+
+
